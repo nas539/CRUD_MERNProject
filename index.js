53cf3472093e78bf5340c9c106fc30084e9af6d8
Nicholas Sharp <naas8905@gmail.com>
2020-06-23 19:06:32 -0700

    Finalizing some details using express

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname,'client', 'build', 'index.html' ));
+    })
+}
+
+
 const employee = require('./routes/employee');
 app.use('/employee', employee);
-mongoose.connect('mongodb://localhost:27017/mernstack', {
+
+const uri = process.env.mongodb || 'mongodb://localhost:27017/mernstack';
+mongoose.connect(uri, {
     useNewUrlParser = true,
     useFindAndModify: false
 },(err) => {
